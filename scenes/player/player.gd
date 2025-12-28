8f624854550a2eeb1e481e10849217f9ef85fcad
DaVidonki_13 <amsalvadori49@gmail.com>
2025-12-28 20:18:36 +0100

    ui and better player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -27,37 +27,45 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
+	if get_slide_collision_count() > 0:
+		speed = velocity.dot(-transform.y)
+	
 	update_stack()
 	#print(orientated)
 
 
 func handle_input(delta) -> void:
-	var turn
+	var turn: float = 0.0
 	
 		#sprite.rotate_sprite()
 	
 	
 	if Input.is_action_pressed("key_up"):
 		speed += acceleration * delta
-		turn = Input.get_axis("key_left", "key_right")
+		#turn = Input.get_axis("key_left", "key_right")
 		#if orientated == 0:
 			#orientated = 1
 			#turn = Input.get_axis("key_left", "key_right")
 	elif Input.is_action_pressed("key_down"):
-		speed -= acceleration * delta
-		turn = Input.get_axis("key_right", "key_left")
+		speed -= friction * delta
+		#turn = Input.get_axis("key_right", "key_left")
 		#if orientated == 0:
 			#orientated = -1
 			#turn = Input.get_axis("key_right", "key_left")
 	
-	if Input.is_action_pressed("key_up") or Input.is_action_pressed("key_down"):
-		
-		rotation += turn * turn_speed * delta
-	
 	speed = clamp(speed, -max_speed * 0.5, max_speed)
 	
-	#TODO RISOLVERE PROBLEMA MACCHINA CHE GIRA A VELOCITÀ TROPPO PICCOLE O AL CONTRARIO
-
+	var direction_factor: int = 0
+	
+	if speed > 10:
+		direction_factor = 1
+	elif speed < -10:
+		direction_factor = -1
+	
+	if direction_factor != 0:
+		turn = Input.get_axis("key_left", "key_right")
+		
+		rotation += turn * turn_speed * delta * direction_factor
 
 
 func apply_friction(delta) -> void:
